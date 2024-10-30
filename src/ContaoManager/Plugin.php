2c789eeb750161bc67b82394d70ded4b1602e680
RobinDort <robin@alexanderdort.com>
2024-10-30 09:50:07 +0100

    Initial setup for a new contao plugin named CustomerCoupons

---
+++ src/ContaoManager/Plugin.php
@@ -0,0 +1,33 @@
+<?php
+namespace RobinDort\CustomerCoupons\ContaoManager;
+
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Contao\CoreBundle\ContaoCoreBundle;
+use RobinDort\CustomerCoupons\RobinDortCustomerCouponsBundle;
+
+
+// use Contao\ManagerPlugin\Routing\RoutingPluginInterface;
+// use Symfony\Component\Config\Loader\LoaderResolverInterface;
+// use Symfony\Component\HttpKernel\KernelInterface;
+
+class Plugin implements BundlePluginInterface, RoutingPluginInterface {
+    public function getBundles(ParserInterface $parser): array {
+        return [
+            BundleConfig::create(RobinDortCustomerCouponsBundle::class)
+                ->setLoadAfter([
+                    ContaoCoreBundle::class,
+                    'isotope',
+                ]),
+            ];
+    }
+
+    // public function getRouteCollection(LoaderResolverInterface $resolver, KernelInterface $kernel)
+    // {
+    //     $file = __DIR__.'/../../config/routes.yaml';
+    //     return $resolver->resolve($file)->load($file);
+    // }
+}
+
+?>
---
+++ src/RobinDortCustomerCouponsBundle.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace RobinDort\CustomerCoupons;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class RobinDortCustomerCouponsBundle extends Bundle {
+    
+}
+
+?>
